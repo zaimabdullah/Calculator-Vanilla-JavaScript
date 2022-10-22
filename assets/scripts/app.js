dfbb32f103583982674331852b0580613fb7a69f
Husni Zaim <zcreative14@gmail.com>
2022-10-22 15:39:14 +0800

    Modified app.js calculator has  working

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,45 @@
 // this is the script that i build
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-currentResult = currentResult + 10; // dummy result here, userInput
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
 
-outputResult(currentResult, '');
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); //use of template literal
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber; //or use +userInput to convert string to int/float
+  createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function subtract(params) {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber; 
+  createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply(params) {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber; 
+  createAndWriteOutput('*', initialResult, enteredNumber); 
+}
+
+function divide(params) {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber); 
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
